9c39eabfb872d640a87a82195f5b0c7919eda49c
HarshaKadekar <harsha.kadekar@asu.edu>
2015-10-13 12:31:20 -0700

    Tried to integrate hashtag generator and tweet fetch
    Still some problems are there. But some tweets it was able to fetch.

--- SourceCode/Tweet_Fetch/HashTagReceiver.py
+++ SourceCode/Tweet_Fetch/HashTagReceiver.py
@@ -1,19 +1,26 @@
-import sys
 from Tweet_Fetch import *
+from TweetViz_Hashtag_Generator import TweetVizHashTagGenerator
 
 consumer_key = 'WQGD1vxJe2COJntOcbq1L7ydH'
 consumer_secret = 'O6MiuVHUpAgHwBEpsOyzSSfNMvIFdsAX0xhmOrmhKmrDnzxYUo'
 
+
 def main_fun():
-	'collects hashtag without hash'
-	hashtags = []
-	index = 0
-	for hashtag in sys.argv[1:]:
-		string_hashtag = '#' + str(hashtag)
-		hashtags.append(string_hashtag)
+    # 'collects hashtag without hash'
+    Finalhashtags = []
+    sentence = "SunDevil is awesome"
+    hashtagsgen = TweetVizHashTagGenerator()
+    hashtagsgen.user_statement = sentence
+    hashtags = hashtagsgen.hash_tag_generator()
 
-	'create an object of Tweet_Fetch class and instantiate the variables'
-	tweet_fetcher = TweetFetcher(consumer_key, consumer_secret)
-	tweet_fetcher.get_tweets(hashtags)
+    for hashtag in hashtags:
+        string_hashtag = '#' + str(hashtag)
+        Finalhashtags.append(string_hashtag)
+    # 'create an object of Tweet_Fetch class and instantiate the variables'
+    tweet_fetcher = TweetFetcher(consumer_key, consumer_secret)
+    print(tweet_fetcher.get_tweets(Finalhashtags))
+    pass
 
-main_fun()
+
+if __name__ == '__main__':
+    main_fun()
--- SourceCode/Tweet_Fetch/Tweet_Fetch.py
+++ SourceCode/Tweet_Fetch/Tweet_Fetch.py
@@ -3,20 +3,20 @@ Developer Gowtham
 Date 12 OCT 2015
 '''
 import tweepy
-import sys
+
 
 class TweetFetcher:
-	def __init__(self, consumer_key, consumer_secret):
-		self.consumer_key = consumer_key
-		self.consumer_secret = consumer_secret
-		self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-		self.api = tweepy.API(self.auth)
+    def __init__(self, consumer_key, consumer_secret):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.api = tweepy.API(self.auth)
 
-	def get_tweets(self, parsed_hashtags):
-		hashtag_count = len(parsed_hashtags)
-		'an implememtation to handle multiple hashtags'
-		for tags in parsed_hashtags:
-			return_result = self.api.search(tags)
-			print tags
-			for tweets in return_result:
-				print tweets.text
+    def get_tweets(self, parsed_hashtags):
+        # hashtag_count = len(parsed_hashtags)
+        # 'an implememtation to handle multiple hashtags'
+        for tags in parsed_hashtags:
+            return_result = self.api.search(tags)
+            print tags
+            for tweets in return_result:
+                print tweets.text
